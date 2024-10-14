f2835b258bed47c3a6698f0847f3e952ce72ac94
SachinBishnoi-503 <bishnoisachin626@gmail.com>
2024-10-14 12:57:21 +0530

    more use magic ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
-import Image from "next/image";
 import { MarqueeDemo } from "../components/magic/MagicUi";
 import { Marquee3D } from "@/components/magic/XToY";
 import { IconCloudDemo } from "@/components/magic/Cloud";
 import { OrbitingCirclesDemo } from "@/components/magic/Circle";
+import { DockDemo } from "@/components/magic/Dock";
+import { AnimatedCircularProgressBarDemo } from "@/components/magic/CircularBar";
+import { AnimatedBeamDemo } from "@/components/magic/Beam";
+import { BoxRevealDemo } from "@/components/magic/Reveal";
 
 export default function Home() {
   return (
     <>
+      <AnimatedCircularProgressBarDemo />
+      <DockDemo />
+      <AnimatedBeamDemo />
+      <BoxRevealDemo/>
       <MarqueeDemo />
       <Marquee3D />
       <IconCloudDemo />
